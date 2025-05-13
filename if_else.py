833509c4bf09246255999f1dde80ce7de6c1280a
unknown <raviharshaarem77@gmail.com>
2025-05-13 21:17:21 +0530

    Commiting if_else

---
+++ if_else.py
@@ -0,0 +1,26 @@
+# a = int(input("Enter the value of a: "))
+# if a > 5:
+# print("a is greater than 5")
+# else:
+# print("a is less than 5")
+
+# if a == 7:
+# print("7 is the jersey number for ms dhoni")
+# elif a == 99:
+# print("99 is the jersey number for gayle")
+# else:
+# print("it is not a jersey number")
+# if 10 == 10:
+# print("Welcome to coding ninjas")
+# Find out area
+x = input("Enter the values: ")
+l, b = x.split()
+l = int(l)
+b = int(b)
+a = l * b
+print(a)
+a = 2
+c = 3
+a, c = c, a
+print(a)
+print(c)
